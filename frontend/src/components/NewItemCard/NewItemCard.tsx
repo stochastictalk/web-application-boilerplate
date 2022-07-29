8206572d98098be4ed5e1bab3a223c159906f8df
Jerome Wynne <jeromewynne@Jeromes-MacBook-Pro.local>
2022-07-29 10:20:54 +0100

    Remove unnecessary useState import.

--- frontend/src/components/NewItemCard/NewItemCard.tsx
+++ frontend/src/components/NewItemCard/NewItemCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 
 import DescriptionInput from "../DescriptionInput/DescriptionInput";
 import AddItemButton from "../AddItemButton/AddItemButton";
